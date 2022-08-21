ec6c7ececf313bdbe0bc905559694be18fa84178
fad16papa <fad16papa@gmail.com>
2022-08-21 18:16:08 +0800

    Commit changes in server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
+//import routes
 const authRoutes = require("./routes/auth.js");
 
+//middlewares
 app.use("/api", authRoutes);
 
-const port = process.env.port || 8000;
+//app middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+//app.use(cors()); //allow all origings
+if ((process.env.NODE_ENV = "development")) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+//connect to database
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`API is running at port ${port}`);
+  console.log(`API is running at port ${port} - $${process.env.NODE_ENV}`);
 });
