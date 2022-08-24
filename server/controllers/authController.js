72ed0843de875d7ab4294d0bb2c638e70884a26e
fad16papa <fad16papa@gmail.com>
2022-08-25 01:08:13 +0800

    Commit chnages

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,35 +40,43 @@ const signup = asyncHandler(async (req, res) => {
   User.findOne({ email }).exec((err, user) => {
     if (user) {
       return res.status(400).json({
-        error: "Email is taken, Please provide another email.",
+        error: "Email is taken",
       });
     }
 
     let token = jwt.sign(
       { name, email, password },
       process.env.JWT_ACCOUNT_ACTIVATION,
-      { expiresIn: "30m" }
+      { expiresIn: "10m" }
     );
 
     let emailData = {
       from: process.env.EMAIL_FROM,
       to: email,
       subject: `Account activation link`,
-      html: `<h1>Please use the following link to activate your account</h1>
-        <p>${process.env.CLIENT_URL}</p>
-        <hr />
-        <p>This email may contain sensitive information.</p>
-        <p>${process.env.CLIENT_URL}</p>
-        `,
+      html: `
+              <h1>Please use the following link to activate your account</h1>
+              <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+              <hr />
+              <p>This email may contain sensetive information</p>
+              <p>${process.env.CLIENT_URL}</p>
+          `,
     };
 
-    sendgridMail.send(emailData).then((sent) => {
-      // console.log("SIGNUP EMAIL SENT", sent);
-
-      return res.json({
-        message: `Email has been sent to ${email}. Please follow the instruction to activate your account.`,
+    sendgridMail
+      .send(emailData)
+      .then((sent) => {
+        // console.log('SIGNUP EMAIL SENT', sent)
+        return res.json({
+          message: `Email has been sent to ${email}. Follow the instruction to activate your account`,
+        });
+      })
+      .catch((err) => {
+        // console.log("SIGNUP EMAIL SENT ERROR", err);
+        return res.json({
+          message: err.message,
+        });
       });
-    });
   });
 });
 
